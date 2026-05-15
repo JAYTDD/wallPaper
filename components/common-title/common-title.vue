6b423ca66fc48946bccaf992c0361176e04bc0f8
JAYTDD <363807870@qq.com>
2026-05-16 00:06:55 +0800

    feat: 初始化uniapp项目，添加基础配置与资源文件
    初始化uniapp项目，添加了.gitignore配置文件，引入uni-icons和uni-scss依赖模块，创建项目基础入口文件App.vue、main.js、pages.json等，新增公共样式common.scss，添加首页示例页面与通用标题组件，同时补充了项目所需的各类静态图片资源。

---
+++ components/common-title/common-title.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="common-title">
+		<view class="left">
+			1111
+		</view>
+		
+		<view class="right">
+			222
+		</view>
+	</view>
+	
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss">
+	.common-title{
+		width: 690rpx;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 30rpx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="homelayout">	
+		<view class="banner">
+			<swiper indicator-dots indicator-active-color= "rgba(255,255,255,0.5)"
+				autoplay circular>
+				<swiper-item v-for="item in 3">
+					<image src="/common/wallpaper/banner1.jpg" mode=""></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		<view class="notice">
+				<view class="left">
+					<uni-icons type="sound" size="20" color="#28b389"></uni-icons>
+					<text class="text">公告</text>
+				</view>
+				<view class="center">
+					<swiper vertical interval="1500" autoplay duration="300" circular >
+						<swiper-item v-for="item in 3">1111111111111111111111111111111111111111111111111111111111111</swiper-item>	
+					</swiper>
+				</view>
+				<view class="right">
+					<uni-icons type="right" size="20" color="#28b389"></uni-icons>
+				</view>
+		</view>
+		
+		<view class="select">
+			<common-title>1111111</common-title>
+			<view class="content">
+				<scroll-view scroll-x >
+					<view class="box" v-for="item in 7">
+						<image src="/common/wallpaper/preview2.jpg" mode=""></image>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped> 
+	.homelayout{
+		
+		.banner{
+			padding: 20rpx 0;
+			width: 750rpx;
+			swiper{
+				width: 750rpx;
+				height: 340rpx;
+				swiper-item{
+				padding: 0 30rpx;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+		}
+		.notice{
+			width: 690rpx;
+			height: 80rpx;
+			line-height: 80rpx;
+			background-color: #f9f9f9;
+			padding: 0 30rpx;
+			margin: auto;
+			border-radius: 80rpx;
+			display: flex;
+			.left{
+				display: flex;
+				justify-content: center;
+				width: 120rpx;
+				.text{
+					color: #28b389;
+					font-size: 28rpx;
+					font-weight: 600;
+				}
+			}
+			.center{
+				height: 100%;
+				flex: 1;
+				swiper{
+					height: 100%;
+					swiper-item{
+						width: 100%;
+						height: 100%;
+						font-size: 30rpx;
+						color: #666;
+						display: block;
+						overflow: hidden; //超出部分隐藏起来
+						text-overflow: ellipsis; //超出部分省略号
+						white-space: nowrap; //不换行
+					}
+				}
+			}
+			.right{
+				display: flex;
+				justify-content: center;
+				width: 70rpx;
+			}
+		}
+		.select{
+			padding-top: 50rpx;
+			.content{
+				width: 720rpx;
+				margin-top: 30rpx;
+				margin-left: 30rpx;
+				scroll-view{
+					white-space: nowrap;
+					.box{
+						display: inline-block;
+						width: 200rpx;
+						height: 430rpx;
+						margin-right: 15rpx;
+						image{
+							width: 100%;
+							height: 100%;
+						}
+					}
+					.box:last-child{
+						margin-right: 30rpx;
+					}
+				}
+			}
+		}
+	}
+	
+</style>
